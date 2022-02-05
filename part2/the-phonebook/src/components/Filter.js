5a8447a885bbc64601387780d61f2f248421a95f
Mohamad Krayem <mk2242682@gmail.com>
2022-02-05 11:32:11 +0200

    the-phonebook final result

--- part2/the-phonebook/src/components/Filter.js
+++ part2/the-phonebook/src/components/Filter.js
@@ -1,7 +1,7 @@
 const Filter = (props) => {
   return (
     <div>
-      <input value={props.value} onChange={props.onChange} />
+      filter shown with<input value={props.value} onChange={props.onChange} />
       {props.array.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
